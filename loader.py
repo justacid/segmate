9b7b0ad39fdac04b6d137a8b385a8ba63cf9f0f3
Tobias Schwackenhofer <tobiasschw@gmail.com>
2018-12-20 14:55:21 +0100

    Feature: Add transparency masks in data loader

---
+++ loader.py
@@ -0,0 +1,48 @@
+from os import listdir
+from pathlib import Path
+
+import imageio as io
+import numpy as np
+from PySide2.QtGui import *
+
+import util
+
+
+class DataLoader:
+
+    def __init__(self, folder):
+        self.root = Path(folder)
+        self.folders = ["images", "masks", "spores"]
+
+        self.files = [f for f in listdir(self.root / self.folders[0])]
+        self.files = sorted(self.files, key=lambda f: int(f.split("-")[1].split(".")[0]))
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, idx):
+        assert idx >= 0 and idx < len(self.files)
+
+        data = [
+            self.load_image(idx),
+            self.load_mask(idx, self.folders[1], color=(255, 255, 0)),
+            self.load_mask(idx, self.folders[2], color=(0, 255, 255)),
+        ]
+
+        return data
+
+    def load_image(self, idx):
+        path = self.root / self.folders[0] / self.files[idx]
+        return QPixmap(util.np_to_qimage(io.imread(path)))
+
+    def load_mask(self, idx, folder, color=(255, 255, 255)):
+        path = self.root / folder / self.files[idx]
+        mask = io.imread(path, as_gray=True)
+        image = self.make_alpha(mask, color=color)
+        return QPixmap(util.np_to_qimage(image))
+
+    def make_alpha(self, image, color=(255, 255, 255)):
+        alpha = np.zeros([*image.shape, 4], dtype=np.uint8)
+        alpha[image == 0, :] = (0, 0, 0, 0)
+        alpha[image == 255, :] = (*color, 255)
+        return alpha
--- store.py
+++ store.py
@@ -1,13 +1,5 @@
-from pathlib import Path
-from os import listdir
-
-import numpy as np
-import imageio as io
 from PySide2.QtCore import Signal
 from PySide2.QtWidgets import QGraphicsScene
-from PySide2.QtGui import QImage, QPixmap
-
-import util
 
 
 class ImageScene(QGraphicsScene):
@@ -45,26 +37,6 @@ class ImageScene(QGraphicsScene):
     def load(self, idx):
         self.clear()
         self.layers = [self.addPixmap(i) for i in self.loader[idx]]
+        for layer, opacity in zip(self.layers, self.opacities):
+            layer.setOpacity(opacity)
         self.image_loaded.emit(idx)
-
-
-class DataLoader:
-
-    def __init__(self, folder, subfolders=None):
-        self.folder = Path(folder)
-        if subfolders is None:
-            self.subfolders = listdir(self.folder)
-        else:
-            self.subfolders = [Path(s) for s in subfolders]
-
-        self.files = [f for f in listdir(self.folder / self.subfolders[0])]
-        self.files = sorted(self.files, key=lambda f: int(f.split("-")[1].split(".")[0]))
-
-    def __len__(self):
-        return len(self.files)
-
-    def __getitem__(self, idx):
-        assert idx >= 0 and idx < len(self.files)
-        paths = [self.folder / s / self.files[idx] for s in self.subfolders]
-        data = [QPixmap(util.np_to_qimage(io.imread(p))) for p in paths]
-        return data
