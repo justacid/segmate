10fd163849f3f67d64573139380379ba620aa4e4
Tobias Schwackenhofer <tobiasschw@gmail.com>
2018-12-20 23:59:47 +0100

    UI: Add visibility toggle

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
+import os
 import sys
+
 from PySide2.QtWidgets import QApplication
 from mainwindow import MainWindow
 
@@ -6,14 +8,15 @@ from mainwindow import MainWindow
 MAJOR_VERSION = 0
 MINOR_VERSION = 0
 PATCH_VERSION = 1
-                                                     
+
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    real_path = os.path.dirname(os.path.realpath(__file__))
+    os.chdir(real_path)
 
+    app = QApplication(sys.argv)
     window = MainWindow()
     version = f"{MAJOR_VERSION}.{MINOR_VERSION}.{PATCH_VERSION}"
     window.setWindowTitle(f"Segmate {version}")
     window.show()
-
     sys.exit(app.exec_())
--- src/widgets/layerwidget.py
+++ src/widgets/layerwidget.py
@@ -1,5 +1,6 @@
 from PySide2.QtCore import *
 from PySide2.QtWidgets import *
+from PySide2.QtGui import *
 
 
 class LayerWidget(QFrame):
@@ -8,6 +9,9 @@ class LayerWidget(QFrame):
 
     def __init__(self, text, opacity=1.0):
         super().__init__()
+
+        self.opacity = opacity
+        self.is_visible = True
 
         self.setFrameShape(QFrame.StyledPanel)
         self.setLineWidth(1)
@@ -21,7 +25,10 @@ class LayerWidget(QFrame):
         hbox.setContentsMargins(5, 2, 5, 3)
 
         self.button = QToolButton(self)
-        self.button.setCheckable(False)
+        self.action = QAction()
+        self.action.setIcon(QIcon("icons/visible.png"))
+        self.action.triggered.connect(self.toggleVisibility)
+        self.button.setDefaultAction(self.action)
         hbox.addWidget(self.button)
 
         svbox = QVBoxLayout()
@@ -29,15 +36,39 @@ class LayerWidget(QFrame):
         self.label.setText(text)
         svbox.addWidget(self.label)
 
-        slider = QSlider(self)
-        slider.setMaximum(100)
-        slider.setValue(int(opacity * 100))
-        slider.setOrientation(Qt.Horizontal)
-        slider.setTickPosition(QSlider.TicksAbove)
-        slider.setTickInterval(20)
-        slider.valueChanged.connect(
-            lambda x: self.opacity_changed.emit(x / 100))
+        self.slider = QSlider(self)
+        self.slider.setMaximum(100)
+        self.slider.setValue(int(opacity * 100))
+        self.slider.setOrientation(Qt.Horizontal)
+        self.slider.valueChanged.connect(self.changeOpacity)
 
-        svbox.addWidget(slider)
+        svbox.addWidget(self.slider)
         hbox.addLayout(svbox)
         vbox.addLayout(hbox)
+
+    def changeOpacity(self, value):
+        self.is_visible = True
+        self.opacity = value / 100
+
+        if self.opacity == 0:
+            self.is_visible = False
+            self.action.setIcon(QIcon("icons/invisible.png"))
+        else:
+            self.action.setIcon(QIcon("icons/visible.png"))
+
+        self.opacity_changed.emit(self.opacity)
+
+    def toggleVisibility(self):
+        self.is_visible = not self.is_visible
+
+        self.slider.blockSignals(True)
+        value = 0
+        if self.is_visible:
+            value = 100 if self.opacity == 0 else int(self.opacity * 100)
+            self.action.setIcon(QIcon("icons/visible.png"))
+        else:
+            self.action.setIcon(QIcon("icons/invisible.png"))
+
+        self.slider.setValue(value)
+        self.opacity_changed.emit(value / 100)
+        self.slider.blockSignals(False)
