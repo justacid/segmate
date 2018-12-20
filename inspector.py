72845bdf766d6349074992c4404ef3d5c868a060
Tobias Schwackenhofer <tobiasschw@gmail.com>
2018-12-20 13:59:35 +0100

    UI: Show current image number and layer names

--- inspector.py
+++ inspector.py
@@ -34,6 +34,8 @@ class Inspector(QWidget):
 
     def changeImage(self, idx):
         self.scene.load(idx)
+        num_images = self.scene.numImages()
+        self.slider_box.setTitle(f"Image {idx+1}/{num_images}")
         self.image_changed.emit(idx)
 
     def clearLayers(self):
@@ -44,23 +46,23 @@ class Inspector(QWidget):
 
     def addLayers(self):
 
-        def add(idx, layer):
-            opacity = self.scene.opacities[idx]
-            self.scene.setOpacity(idx, opacity)
-            item = LayerItem(f"Opacity: Layer #{idx}", opacity)
+        def add(idx, layer, name):
+            opacity = self.scene.getOpacity(idx)
+            text = f"Opacity: {name}".title()
+            item = LayerItem(text, opacity)
             item.opacity_changed.connect(lambda x: self.scene.setOpacity(idx, x))
             self.layers.addWidget(item)
 
         self.clearLayers()
-        for i, layer in enumerate(self.scene.layers):
-            add(i, layer)
+        for i, (layer, name) in enumerate(zip(self.scene.layers, self.scene.loader.folders)):
+            add(i, layer, name)
         self.layers.addItem(QSpacerItem(1, 100, QSizePolicy.Minimum, QSizePolicy.Expanding))
 
     def setupUi(self):
         dock_layout = QVBoxLayout(self)
 
-        slider_group = QGroupBox("Images")
-        vlayout = QVBoxLayout(slider_group)
+        self.slider_box = QGroupBox("Images")
+        vlayout = QVBoxLayout(self.slider_box)
 
         self.slider = QSlider(Qt.Horizontal)
         self.slider.setTickPosition(QSlider.TicksBelow)
@@ -69,7 +71,7 @@ class Inspector(QWidget):
         self.slider.valueChanged.connect(self.changeImage)
 
         vlayout.addWidget(self.slider)
-        dock_layout.addWidget(slider_group)
+        dock_layout.addWidget(self.slider_box)
 
         layer_group = QGroupBox(self)
         layer_group.setTitle("Layers")
