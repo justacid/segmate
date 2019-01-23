21abfc46c5d6f624313b325d5349f5a1964253a6
Tobias Schwackenhofer <tobiasschw@gmail.com>
2019-01-23 17:22:15 +0100

    UI/Tools: Add framework for rectangular selections

---
+++ segmate/editor/selection.py
@@ -0,0 +1,117 @@
+import numpy as np
+from PySide2.QtCore import *
+from PySide2.QtWidgets import *
+from PySide2.QtGui import *
+import segmate.util as util
+
+
+class RectSelection:
+
+    def __init__(self, item=None):
+        self._selecting = False
+        self._corner1 = None
+        self._corner2 = None
+
+        self.item = item
+        if self.item is not None:
+            self.item_mouse_pressed = self.item.mouse_pressed
+            self.item_mouse_released = self.item.mouse_released
+            self.item_mouse_moved = self.item.mouse_moved
+
+            self.item.mouse_pressed = self.__mouse_pressed
+            self.item.mouse_released = self.__mouse_released
+            self.item.mouse_moved = self.__mouse_moved
+
+    @property
+    def is_active(self):
+        if self._corner1 and self._corner2:
+            return True
+
+    @property
+    def rect(self):
+        x1, x2 = int(self._corner1.x()), int(self._corner2.x())
+        y1, y2 = int(self._corner1.y()), int(self._corner2.y())
+        if x1 > x2 and y1 > y2:
+            return [(x2, y2), (x1, y1)]
+        elif x1 < x2 and y1 > y2:
+            return [(x1, y2), (x2, y1)]
+        elif x1 > x2 and y1 < y2:
+            return [(x2, y1), (x1, y2)]
+        elif x1 < x2 and y1 < y2:
+            return [(x1, y1), (x2, y2)]
+
+    @property
+    def indices(self):
+        if not self.rect:
+            return None
+        c1, c2 = self.rect
+        xs = np.arange(c1[0], c2[0], dtype=np.int32)
+        ys = np.arange(c1[1], c2[1], dtype=np.int32)
+        return tuple(np.meshgrid(ys, xs, indexing="ij"))
+
+    def start(self, pos):
+        self._selecting = True
+        self._corner1 = pos
+        self._corner2 = None
+
+    def move(self, pos):
+        if self._selecting:
+            self._corner2 = pos
+
+    def release(self, pos):
+        if self._selecting:
+            self._selecting = False
+
+    def reset(self):
+        self._selecting = False
+        self._corner1 = None
+        self._corner2 = None
+
+    def paint(self, canvas):
+        if not self.is_active:
+            return
+        if not self.indices:
+            return
+
+        try:
+            image = util.from_qimage(canvas)
+            mask = np.zeros(image.shape[:2], dtype=np.bool)
+            mask[self.indices] = True
+
+            is_neg = self.indices[0] < 0
+            js_neg = self.indices[1] < 0
+            if is_neg.any() or js_neg.any():
+                self.reset()
+                return
+
+            image[~mask] = (0, 0, 0, 96)
+            image = util.to_qimage(image)
+        except:
+            self.reset()
+            return
+
+        painter = QPainter(canvas)
+        painter.drawImage(0, 0, image)
+        painter.setPen(QPen(QColor(47, 79, 79)))
+        painter.drawRect(QRectF(self._corner1, self._corner2))
+
+    def __mouse_pressed(self, event):
+        if event.button() == Qt.LeftButton:
+            self.start(event.pos())
+            return True
+        if event.button() == Qt.RightButton:
+            self.reset()
+            return True
+        return self.item_mouse_pressed(event)
+
+    def __mouse_released(self, event):
+        if event.button() == Qt.LeftButton:
+            self.release(event.pos())
+            return True
+        return self.item_mouse_released(event)
+
+    def __mouse_moved(self, event):
+        if event.buttons() & Qt.LeftButton:
+            self.move(event.pos())
+            return True
+        return self.item_mouse_moved(event)
--- segmate/editor/tools/masks.py
+++ segmate/editor/tools/masks.py
@@ -5,12 +5,13 @@ from PySide2.QtGui import *
 
 from segmate.editor.tools.editortool import EditorTool
 from segmate.editor.toolwidget import EditorToolWidget
+from segmate.editor.selection import RectSelection
 import segmate.util as util
 
 
 class MasksToolInspector(EditorToolWidget):
 
-    def __init__(self, copy_cb, clear_cb, merge_cb, cpy_merge_cb):
+    def __init__(self, copy_cb, clear_cb, merge_cb, clr_merge_cb):
         super().__init__("Masks")
         copy_button = QPushButton("Copy Previous Mask")
         copy_button.pressed.connect(copy_cb)
@@ -18,18 +19,24 @@ class MasksToolInspector(EditorToolWidget):
         clear_button.pressed.connect(clear_cb)
         merge_button = QPushButton("Merge Masks")
         merge_button.pressed.connect(merge_cb)
-        cpy_merge_btn = QPushButton("Copy+Merge Masks")
-        cpy_merge_btn.pressed.connect(cpy_merge_cb)
+        clr_merge_btn = QPushButton("Clear && Merge Masks")
+        clr_merge_btn.pressed.connect(clr_merge_cb)
         self.add_widget(copy_button)
         self.add_widget(clear_button)
         self.add_widget(merge_button)
-        self.add_widget(cpy_merge_btn)
+        self.add_widget(clr_merge_btn)
 
 
 class MasksTool(EditorTool):
 
+    def __init__(self, image, item):
+        super().__init__(image, item)
+        self._selection = RectSelection(self)
+
     def paint_canvas(self):
-        return self.canvas
+        image = QImage(self.canvas)
+        self._selection.paint(image)
+        return image
 
     def paint_result(self):
         return self.canvas
@@ -38,31 +45,55 @@ class MasksTool(EditorTool):
         self.is_dirty = True
         idx = max(self.item.image_idx - 1, 0)
         layer = self.item.layer_idx
-        mask = self.item.scene.data_loader[idx][layer]
-        self.push_undo_snapshot(self.canvas, mask, undo_text="Copy Mask")
-        self.canvas = QImage(mask)
+        mask = util.extract_binary_mask(self.item.scene.data_loader[idx][layer])
+        current_mask = util.extract_binary_mask(self.canvas)
+
+        output = np.zeros(mask.shape, dtype=np.uint8)
+        if self._selection.is_active:
+            sel_mask = np.zeros(mask.shape, dtype=np.bool)
+            sel_mask[self._selection.indices] = True
+            output[current_mask == 1] = 1
+            output[(mask == 1) & (sel_mask == 1)] = 1
+        else:
+            output[mask == 1] = 1
+        output = util.color_binary_mask(output, self.pen_color)
+
+        self.push_undo_snapshot(self.canvas, output, undo_text="Copy Mask")
+        self.canvas = output
 
     def _clear_mask(self):
-        mask = np.zeros((self.canvas.height(), self.canvas.width()), dtype=np.uint8)
-        mask = util.color_binary_mask(mask, self.pen_color)
-        self.push_undo_snapshot(self.canvas, mask, undo_text="Clear Mask")
-        self.canvas = mask
+        self.is_dirty = True
+        mask = util.extract_binary_mask(self.canvas)
+        if self._selection.is_active:
+            mask[self._selection.indices] = 0
+        else:
+            mask = np.zeros(mask.shape, dtype=np.uint8)
+
+        image = util.color_binary_mask(mask, self.pen_color)
+        self.push_undo_snapshot(self.canvas, image, undo_text="Clear Mask")
+        self.canvas = image
 
     def _merge_masks(self):
         self.is_dirty = True
-
         idx = self.item.image_idx
         layer = self.item.layer_idx
 
+        current_mask = util.extract_binary_mask(self.canvas)
         output = np.zeros((self.canvas.height(), self.canvas.width()), dtype=np.uint8)
         for i in range(1, len(self.item.scene.layers)):
             mask = util.extract_binary_mask(self.item.scene.data_loader[idx][i])
-            output[mask == 1] = 1
+            if self._selection.is_active:
+                sel_mask = np.zeros(mask.shape, dtype=np.bool)
+                sel_mask[self._selection.indices] = True
+                output[(mask == 1) & (sel_mask == 1)] = 1
+                output[current_mask == 1] = 1
+            else:
+                output[mask == 1] = 1
         output = util.color_binary_mask(output, self.pen_color)
         self.push_undo_snapshot(self.canvas, output, undo_text="Merge Mask")
         self.canvas = output
 
-    def _cpy_merge(self):
+    def _clr_merge(self):
         self._clear_mask()
         self._merge_masks()
 
@@ -71,4 +102,4 @@ class MasksTool(EditorTool):
         if not self.is_editable:
             return None
         return MasksToolInspector(
-            self._copy_previous_mask, self._clear_mask, self._merge_masks, self._cpy_merge)
+            self._copy_previous_mask, self._clear_mask, self._merge_masks, self._clr_merge)
