c4c29dc40f201c6b6c3a9ba7d35b50684d5f6642
Tobias Schwackenhofer <tobiasschw@gmail.com>
2019-02-12 22:24:48 +0100

    Packaging: Minor fixes

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ import segmate
 
 
 setuptools.setup(
-    name="Segmate",
+    name="segmate",
     version=segmate.__version__,
     python_requires=">=3.6",
     description="A tool to annotate segmentation data sets.",
     packages=setuptools.find_packages(),
     install_requires=[
         "PySide2==5.12.0",
-        "scikit-image==0.14.2",
-        "imageio==2.4.1"
+        "scikit-image>=0.14.2",
+        "imageio>=2.4.1"
     ],
     include_package_data=True,
     entry_points = {
