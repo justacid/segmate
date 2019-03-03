dcb8667d5e22f70c8729fbaa65be8b1431405a66
Tobias Schwackenhofer <tobiasschw@gmail.com>
2019-03-03 18:22:21 +0100

    Make plugin loading more robust
    Ignore (hidden) files in plugins directory, as well as hidden folders.

--- segmate/editor/registry.py
+++ segmate/editor/registry.py
@@ -11,19 +11,30 @@ tools = {}
 data_location = QStandardPaths.standardLocations(QStandardPaths.AppDataLocation)[0]
 plugins_location = pathlib.Path(data_location) / "segmate" / "plugins"
 
-# adding the plugin location to the path,
-# such that the plugin packages can be imported
-sys.path.insert(0, str(plugins_location))
+if plugins_location.exists():
 
-for path in plugins_location.iterdir():
-    module = importlib.import_module(path.parts[-1])
-    plugin_info = [getattr(module, "__plugin_title__", None)]
-    found = False
-    for name, obj in inspect.getmembers(module):
-        if inspect.isclass(obj):
-            if issubclass(obj, EditorTool) and obj != EditorTool:
-                plugin_info.append(obj)
-                found = True
-                break
-    if found:
-        tools[path.parts[-1]] = plugin_info
+    # add the plugins location to the path,
+    # so that it can be found by importlib
+    sys.path.insert(0, str(plugins_location))
+
+    for path in plugins_location.iterdir():
+
+        if not path.is_dir():
+            continue
+
+        # ignore hidden folders
+        if path.parts[-1].startswith("."):
+            continue
+
+        module = importlib.import_module(path.parts[-1])
+        plugin_info = [getattr(module, "__plugin_title__", None)]
+
+        found = False
+        for name, obj in inspect.getmembers(module):
+            if inspect.isclass(obj):
+                if issubclass(obj, EditorTool) and obj != EditorTool:
+                    plugin_info.append(obj)
+                    found = True
+                    break
+        if found:
+            tools[path.parts[-1]] = plugin_info
