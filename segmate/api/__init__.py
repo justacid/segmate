97e06600768964c220802148bf557096091a24f2
Tobias Schwackenhofer <tobiasschw@gmail.com>
2019-06-17 01:32:29 +0200

    Add new api package, exporting stable functions
    This package is a facade and reexports everything that is
    meant to be stable and therefore can be imported freely
    by any plugin.

---
+++ segmate/api/__init__.py
@@ -0,0 +1,6 @@
+from ..editor.editortool import EditorTool as Plugin
+from . import widgets
+from . import util
+from . import draw
+
+__all__ = ["widgets", "util", "draw"]
---
+++ segmate/api/widgets.py
@@ -0,0 +1,4 @@
+from ..editor.widgets.toolwidget import EditorToolWidget as Inspector
+from ..editor.widgets.button import Button
+from ..editor.widgets.labeledcheckbox import LabeledCheckbox
+from ..editor.widgets.labeledslider import LabeledSlider
