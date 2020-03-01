51ccc42ed22c2c7ea8e5815507340b25697e34a5
Tobias Schwackenhofer <tobiasschw@gmail.com>
2020-03-01 12:07:24 +0100

    Update PySide2 dependency for Python 3.8 support

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     description="A tool to annotate segmentation data sets.",
     packages=setuptools.find_packages(),
     install_requires=[
-        "PySide2 == 5.12.3",
+        "PySide2 == 5.14.1",
         "scikit-image == 0.14.2",
         "imageio == 2.4.1",
         "natsort == 6.0.0"
